e0c8a79156c546c8a13d555c7a23afa9b7126f53
Matt Stratton <matt.stratton@gmail.com>
2017-08-08 16:54:23 -0500

    Add basic stuff

--- main.go
+++ main.go
@@ -1,19 +1,44 @@
-// main.go
-
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/mattstratton/blondie/daemon"
 )
 
+var dbhost = os.Getenv("POSTGRES_HOST")
+var port = "5432"
+var user = os.Getenv("POSTGRES_USER")
+var password = os.Getenv("POSTGRES_PASSWORD")
+var dbname = os.Getenv("POSTGREST_DB")
+
+var assetsPath string
+
+func processFlags() *daemon.Config {
+	cfg := &daemon.Config{}
+
+	flag.StringVar(&cfg.ListenSpec, "listen", "localhost:3000", "HTTP listen spec")
+	flag.StringVar(&cfg.Db.ConnectString, "db-connect", "host=db dbname=dev user=postgres-dev password=s3cr3tp4ssw0rd sslmode=disable", "DB Connect String")
+	flag.StringVar(&assetsPath, "assets-path", "assets", "Path to assets dir")
+
+	flag.Parse()
+	return cfg
+}
+
+func setupHTTPAssets(cfg *daemon.Config) {
+	log.Printf("Assets served from %q.", assetsPath)
+	cfg.UI.Assets = http.Dir(assetsPath)
+}
+
 func main() {
+	cfg := processFlags()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from Docker")
-	})
+	setupHTTPAssets(cfg)
 
-	fmt.Println("Listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	if err := daemon.Run(cfg); err != nil {
+		log.Printf("Error in main(): %v", err)
+	}
 }
